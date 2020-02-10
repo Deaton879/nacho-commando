5bc7e4bd9e4c9731cecda2c727453a4855ace5ec
Deaton879 <60494177+Deaton879@users.noreply.github.com>
2020-02-10 15:12:51 -0500

    Add files via upload

---
+++ classes.py
@@ -0,0 +1,37 @@
+class Point:
+    def __init__(self):
+        self.x = 0.0
+        self.y = 0.0
+        
+class Velocity:
+    def __init__(self):
+        self.dx = 0.0
+        self.dy = 0.0
+
+class Ball:
+    def __init__(self):
+        self.center = Point()
+        self.velocity = Velocity()
+        
+    def draw(self):
+        
+    def advance(self):
+        
+    def bounce_horizontal(self):
+        
+    def bounce_vertical(self):
+        
+    def restart(self):
+        
+class Paddle:
+    def __init__(self):
+        self.center = Point()
+    
+    def draw():
+        
+    def move_up(self):
+        
+    def move_down(self):
+        
+    
+        
